215778171f0ab4ecf2ec2f327dfc7f97be96fe9e
Sergey Zveroboev <szveroboev@gmail.com>
2017-09-21 04:35:33 +0300

    Создал статическую структуру React приложения

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const App = (props) => {
+    return (
+        <main>
+            <header>
+                <h1>{props.title}</h1>
+            </header>
+            <section className="todo-list">
+                <div className="todo completed">
+                    <button className="checkbox icon">
+                        <i className="material-icons">check_box</i>
+                    </button>
+
+                    <span className="todo-title">Изучить JavaScript</span>
+
+                    <button className="delete icon">
+                        <i className="material-icons">delete</i>
+                    </button>
+                </div>
+
+                <div className="todo">
+                    <button className="checkbox icon">
+                        <i className="material-icons">check_box_outline_blank</i>
+                    </button>
+
+                    <span className="todo-title">Изучить ES2015</span>
+
+                    <button className="delete icon">
+                        <i className="material-icons">delete</i>
+                    </button>
+                </div>
+
+                <div className="todo">
+                    <button className="checkbox icon">
+                        <i className="material-icons">check_box_outline_blank</i>
+                    </button>
+
+                    <span className="todo-title">Изучить React</span>
+
+                    <button className="delete icon">
+                        <i className="material-icons">delete</i>
+                    </button>
+                </div>
+            </section>
+        </main>
+    );
+};
+
+App.propTypes = {
+    title: PropTypes.string
+};
+App.defaultProps = {
+    title: 'React Todo'
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
